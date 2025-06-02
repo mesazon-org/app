b57c2e77481e77d68b9dc3ca5ef07090530f3719
Andreas Rigas <andreas.rigas@outlook.com>
2025-06-02 23:53:12 +0100

    feat: table creation

--- backend/schemas/migrations/V2025.05.27__init.sql
+++ backend/schemas/migrations/V2025.05.27__init.sql
@@ -1,14 +1,15 @@
-CREATE TABLE rikkos.user_metadata (
-    "user_id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
+CREATE TABLE "user_metadata" (
+    "user_id" TEXT PRIMARY KEY NOT NULL,
+    "email" TEXT NOT NULL UNIQUE,
     "first_name" TEXT NOT NULL,
     "last_name" TEXT NOT NULL,
     "country_code" TEXT NOT NULL,
     "phone_number" TEXT NOT NULL,
-    "address" TEXT NOT NULL,
-    "organization" TEXT NOT NULL,
+    "address_line_1" TEXT NOT NULL,
+    "address_line_2" TEXT,
+    "city" TEXT NOT NULL,
+    "postal_code" TEXT NOT NULL,
+    "company" TEXT NOT NULL,
     "created_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    "updated_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    CONSTRAINT "member_pk" PRIMARY KEY("member_id")
-    UNIQUE("email")
+    "updated_at" TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
