b1e1ec7210df19efcc7bbc64333c91e5b6cddd09
Andreas Rigas <andreas.rigas@outlook.com>
2025-06-24 22:48:53 +0100

    update both tables

--- backend/schemas/migrations/V2025.05.27__init.sql
+++ backend/schemas/migrations/V2025.05.27__init.sql
@@ -1,33 +1,36 @@
-CREATE TABLE "users_details" (
-    "user_id" TEXT PRIMARY KEY NOT NULL,
-    "email" TEXT NOT NULL UNIQUE,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT NOT NULL,
-    "country_code" TEXT NOT NULL,
-    "phone_number" TEXT NOT NULL,
-    "address_line_1" TEXT NOT NULL,
-    "address_line_2" TEXT,
-    "city" TEXT NOT NULL,
-    "postal_code" TEXT NOT NULL,
-    "company" TEXT NOT NULL,
-    "created_at" TIMESTAMPTZ NOT NULL,
-    "updated_at" TIMESTAMPTZ NOT NULL
+CREATE TABLE users_details (
+    user_id TEXT PRIMARY KEY NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    country_code TEXT NOT NULL,
+    phone_number TEXT NOT NULL,
+    address_line_1 TEXT NOT NULL,
+    address_line_2 TEXT,
+    city TEXT NOT NULL,
+    postal_code TEXT NOT NULL,
+    company TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL
 );
 
-CREATE TABLE "users_contacts" (
-    "contact_id" TEXT PRIMARY KEY NOT NULL,
-    "user_id" TEXT NOT NULL REFERENCES users_details("user_id"),
-    "display_name" TEXT NOT NULL,
-    "first_name" TEXT NOT NULL,
-    "last_name" TEXT,
-    "company" TEXT,
-    "email" TEXT,
-    "country_code" TEXT NOT NULL,
-    "phone_number" TEXT NOT NULL,
-    "address_line_1" TEXT,
-    "address_line_2" TEXT,
-    "city" TEXT,
-    "postal_code" TEXT,
-    "created_at" TIMESTAMPTZ NOT NULL,
-    "updated_at" TIMESTAMPTZ NOT NULL
+CREATE TABLE users_contacts (
+    contact_id TEXT PRIMARY KEY NOT NULL,
+    user_id TEXT NOT NULL,
+    display_name TEXT NOT NULL,
+    first_name TEXT NOT NULL,
+    last_name TEXT,
+    company TEXT,
+    email TEXT,
+    country_code TEXT NOT NULL,
+    phone_number TEXT NOT NULL,
+    address_line_1 TEXT,
+    address_line_2 TEXT,
+    city TEXT,
+    postal_code TEXT,
+    created_at TIMESTAMPTZ NOT NULL,
+    updated_at TIMESTAMPTZ NOT NULL,
+    CONSTRAINT user_fk FOREIGN KEY(user_id) REFERENCES users_details(user_id)
 );
+
+CREATE INDEX idx_users_contacts_user_id ON users_contacts(user_id);
