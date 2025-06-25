00d92493af8cbf2500c475d2feebd0f2bfba7906
Andreas Rigas <48296471+andrewrigas@users.noreply.github.com>
2025-06-25 22:23:17 +0100

    chore(deps): Update iron, iron-circe to 3.0.2 (#63)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
   lazy val scalacheckV          = "1.18.1"
   lazy val testcontainersScalaV = "0.43.0"
   lazy val testcontainersV      = "1.21.1"
-  lazy val ironV                = "3.0.1"
+  lazy val ironV                = "3.0.2"
   lazy val circeV               = "0.14.10"
   lazy val chimneyV             = "1.8.1"
   lazy val doobieV              = "1.0.0-RC9"
