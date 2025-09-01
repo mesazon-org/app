4811a3469a9538d7b7201db04d0b911537128e68
Andreas Rigas <andreas.rigas@outlook.com>
2025-09-01 01:22:46 +0000

    chore(deps): Update tranzactio-doobie to 5.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
   lazy val doobieV              = "1.0.0-RC9"
   lazy val postgresqlV          = "42.7.3"
   lazy val hikariCPV            = "7.0.2"
-  lazy val doobieTranzactioV    = "5.4.0"
+  lazy val doobieTranzactioV    = "5.5.0"
   lazy val libphonenumberV      = "9.0.13"
   lazy val circeV               = "0.14.14"
 
