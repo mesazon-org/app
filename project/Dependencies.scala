d803e711540806aec2d2c128d70c74ff3d1aa1e1
Andreas Rigas <andreas.rigas@outlook.com>
2026-01-07 01:22:13 +0000

    chore(deps): Update smithy4s-http4s, ... to 0.18.46

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object Dependencies {
 
   lazy val http4sV              = "0.23.33"
-  lazy val smithy4sV            = "0.18.45"
+  lazy val smithy4sV            = "0.18.46"
   lazy val zioV                 = "2.1.24"
   lazy val zioConfigV           = "4.0.6"
   lazy val zioInteropCatsV      = "23.1.0.13"
