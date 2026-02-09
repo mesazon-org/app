4bd6d8f95afdede2589eefaf96ed925182fcfbe2
Andreas Rigas <andreas.rigas@outlook.com>
2026-02-09 23:55:49 +0200

    feat: fix

--- terraform/modules/app-service/main.tf
+++ terraform/modules/app-service/main.tf
@@ -22,11 +22,33 @@ resource "digitalocean_app" "app_service" {
       name               = local.service_name
       instance_count     = var.replicas
       instance_size_slug = var.app_size
+      http_port          = 8080
+
+      internal_ports     = [8081]
 
       image {
         registry_type = var.registry_type
         repository    = var.image_name
         tag           = var.image_tag
+      }
+
+      health_check {
+        http_path             = "/readiness"
+        port                  = 8081
+        initial_delay_seconds = 5
+        period_seconds        = 10
+        timeout_seconds       = 5
+        success_threshold     = 1
+        failure_threshold     = 3
+      }
+
+      liveness_health_check {
+        http_path             = "/liveness"
+        port                  = 8081
+        initial_delay_seconds = 10
+        period_seconds        = 15
+        timeout_seconds       = 5
+        failure_threshold     = 3
       }
 
       dynamic "env" {
@@ -46,13 +68,6 @@ resource "digitalocean_app" "app_service" {
           value = env.value
           type  = "SECRET"
         }
-      }
-
-      health_check {
-        http_path             = "/liveness"
-        port                  = 8081
-        initial_delay_seconds = 5
-        period_seconds        = 10
       }
     }
 
