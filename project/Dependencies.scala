9e0be53fec54ccde734621ea84de2afa3eb3828b
Andreas Rigas <48296471+andrewrigas@users.noreply.github.com>
2026-02-12 16:28:44 +0000

    chore(deps): Update logback-classic to 1.5.29 (#195)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   lazy val zioInteropCatsV      = "23.1.0.13"
   lazy val catsV                = "2.13.0"
   lazy val zioLoggingV          = "2.5.3"
-  lazy val logbackV             = "1.5.28"
+  lazy val logbackV             = "1.5.29"
   lazy val julToSlf4jV          = "2.0.17"
   lazy val pureconfigV          = "0.17.10"
   lazy val scalaTestV           = "3.2.19"
