e1e72b6037fdd47d85fab21b6b37a7955e73d956
Andreas Rigas <andreas.rigas@outlook.com>
2026-08-07 02:53:48 +0000

    chore(deps): Update openai, zio to 0.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -29,7 +29,7 @@ object Dependencies {
   lazy val sttpV                = "4.0.26"
   lazy val tapirV               = "1.13.30"
   lazy val jsoniterV            = "2.39.1"
-  lazy val sttpAIV              = "0.6.0"
+  lazy val sttpAIV              = "0.7.0"
   lazy val jmailV               = "2.2.0"
   lazy val simplejavamailV      = "9.1.3"
   lazy val jjwtV                = "0.13.0"
