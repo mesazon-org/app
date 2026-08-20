81370d8859c1334a39cfa9aa7615a9886b076eac
Andreas Rigas <48296471+andrewrigas@users.noreply.github.com>
2026-08-20 17:02:57 +0100

    chore(deps): Update jsoniter-scala-core, ... to 2.40.1 (#459)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -28,7 +28,7 @@ object Dependencies {
   lazy val circeV               = "0.14.16"
   lazy val sttpV                = "4.0.26"
   lazy val tapirV               = "1.13.31"
-  lazy val jsoniterV            = "2.39.1"
+  lazy val jsoniterV            = "2.40.1"
   lazy val sttpAIV              = "0.8.0"
   lazy val jmailV               = "2.2.0"
   lazy val simplejavamailV      = "9.1.8"
