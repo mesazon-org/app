182227625cb8c0bc4da2085382c812335eaabe1c
Andreas Rigas <48296471+andrewrigas@users.noreply.github.com>
2026-09-13 11:41:23 +0100

    chore(deps): Update http4s-circe, http4s-dsl, ... to 0.23.37 (#510)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt.*
 
 object Dependencies {
 
-  lazy val http4sV              = "0.23.36"
+  lazy val http4sV              = "0.23.37"
   lazy val smithy4sV            = "0.19.12"
   lazy val zioV                 = "2.1.26"
   lazy val zioConfigV           = "4.0.8"
