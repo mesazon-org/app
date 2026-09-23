6a41bb4206995361de2ff0e72e91f71e259f9269
Andreas Rigas <andreas.rigas@outlook.com>
2026-09-23 04:11:38 +0000

    chore(deps): Update commons-csv to 1.14.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,7 +44,7 @@ object Dependencies {
   lazy val awssdkV              = "2.55.1"
   lazy val zioS3V               = "0.4.4"
   lazy val poiV                 = "5.3.0"
-  lazy val commonsCsvV          = "1.12.0"
+  lazy val commonsCsvV          = "1.14.1"
 
   // Http4s
   lazy val http4sDsl         = "org.http4s" %% "http4s-dsl"          % http4sV
